ad930b20d2a983d8c8a14d27f8c512196cf3a8cd
angelArchbold <angelarchbold09@gmail.com>
2020-09-13 12:14:27 -0500

    basedata updated

--- database/data.js
+++ database/data.js
@@ -1,36 +1,39 @@
 const data = {
-  "2zd33b8c": {
-    name: "Postre1",
-    id: "2zd33b8c",
-    sku: "NUR72KCM",
-    price: 1.15,
-    image: "",
-    description:"A relatively new cultivar, it was, the pretty boy baby, discovered in South Africa in the early 1990s by Mr. A.G. (Dries) Joubert. Maluma Babyy. It is a chance seedling of unknown parentage",
-    shape: "Oval",
-    hardiness: "1 °C",
-    taste: "Catchy, is an avocado"
+  "29ac1994-8cf6-4838-9ceb-734df4a6c924": {
+    id: "29ac1994-8cf6-4838-9ceb-734df4a6c924",
+    name: "Sauce - White, Mix",
+    price: 23081,
+    image: "http://dummyimage.com/196x162.bmp/cc0000/ffffff",
   },
-  "2zd33b7c": {
-    name: "Postre2",
-    id: "2zd33b8c",
-    sku: "NUR72KCM",
-    price: 1.15,
-    image: "",
-    description:"A relatively new cultivar, it was, the pretty boy baby, discovered in South Africa in the early 1990s by Mr. A.G. (Dries) Joubert. Maluma Babyy. It is a chance seedling of unknown parentage",
-    shape: "Oval",
-    hardiness: "1 °C",
-    taste: "Catchy, is an avocado"
+  product2: {
+    id: "404812c8-d734-4721-a14b-f9d63c767cec",
+    name: "Carbonated Water - Strawberry",
+    price: 1314,
+    image: "http://dummyimage.com/207x171.bmp/5fa2dd/ffffff",
   },
-  "2zd33b6c": {
-    name: "Postre3",
-    id: "2zd33b8c",
-    sku: "NUR72KCM",
-    price: 1.15,
-    image: "",
-    description:"A relatively new cultivar, it was, the pretty boy baby, discovered in South Africa in the early 1990s by Mr. A.G. (Dries) Joubert. Maluma Babyy. It is a chance seedling of unknown parentage",
-    shape: "Oval",
-    hardiness: "1 °C",
-    taste: "Catchy, is an avocado"
+  product3: {
+    id: "5778e303-9607-4708-be6f-a08f5d0296c4",
+    name: "Thermometer Digital",
+    price: 4209,
+    image: "http://dummyimage.com/124x212.bmp/dddddd/000000",
+  },
+  product4: {
+    id: "981fc573-c999-4dbb-8653-0b991ec2d45a",
+    name: "The Pop Shoppe - Root Beer",
+    price: 17675,
+    image: "http://dummyimage.com/176x118.jpg/dddddd/000000",
+  },
+  product5: {
+    id: "6cf6fe50-d681-4523-9de9-82e6c03c37c0",
+    name: "Daikon Radish",
+    price: 45999,
+    image: "http://dummyimage.com/237x209.png/dddddd/000000",
+  },
+  product6: {
+    id: "a0038a80-7b5c-4971-977c-1ffe498cd7c9",
+    name: "Bread - Corn Muffaleta Onion",
+    price: 37718,
+    image: "http://dummyimage.com/212x186.png/5fa2dd/ffffff",
   },
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
-import { useEffect, useState } from "react";
 import Link from "next/link";
-const Home = () => {
 
-  const [productList, setProductList] = useState([]);
-  useEffect(() => {
-    fetch("/api/avo")
-      .then((response) => response.json())
-      .then(({ data, length }) => {
-        setProductList(data);
-      });
-  }, []);
+export const getStaticProps = async () => {
+  const response = await fetch("https://bamboo-page.vercel.app/api/products");
+  const { data: productList } = await response.json();
 
+  return {
+    props: {
+      productList,
+    },
+  };
+};
+
+const Home = ({ productList }) => {
   return (
     <>
       <main>
@@ -45,24 +46,28 @@ const Home = () => {
                   <figure className="rounded-lg">
                     <img
                       className="w-full px-4 py-4 object-cover"
-                      src="/images/cakes.jpeg"
+                      src={productList.image}
                       alt="cakes-image"
                     />
                   </figure>
                 </div>
                 <div className="px-6 py-4 bg-white bg-white">
                   <h4 className="font-bold text-xl mb-2 font-extrabold text-gray-900">
-                    CUPCAKES
+                    {productList.name}
                   </h4>
-                  <p class="font-extrabold text-gray-700">Precio: $16.000</p>
+                  <p class="font-extrabold text-gray-700">
+                    Precio: ${productList.price}
+                  </p>
                 </div>
                 <div className="px-6 pt-4 pb-2 bg bg-white ">
-                  <button className="bg-green-200 rounded-full px-3 py-2 font-semibold text-green-900 mr-2 mb-2">
-                    Ver más
-                  </button>
+                  <Link href="/product/[productList.link]" as={`/product/${productList.id}`}>
+                    <button className="bg-green-200 rounded-full px-3 py-2 font-semibold text-green-900 mr-2 mb-2">
+                      Ver más
+                    </button>
+                  </Link>
                 </div>
               </div>
-
+              {/* 
               <div className="max-w-sm w-64 rounded overflow-hidden shadow-lg mx-2 my-2 text-center transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-80 transition ease-in-out duration-200 text-gray-900">
                 <div className="bg-green-100">
                   <figure className="rounded-lg">
@@ -130,7 +135,7 @@ const Home = () => {
                     Ver más
                   </button>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </section>
