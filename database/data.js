3eb136652d33acd50a4ccf304df70c202ca1f231
angelArchbold <angelarchbold09@gmail.com>
2020-09-13 18:11:22 -0500

    database update again

--- database/data.js
+++ database/data.js
@@ -1,40 +1,40 @@
 const data = {
-  "29ac1994-8cf6-4838-9ceb-734df4a6c924": {
+  "product1": {
     id: "29ac1994-8cf6-4838-9ceb-734df4a6c924",
     name: "Cookies letters",
     detail:"Galletas personalizadas con cobertura de chocolate.",
     price: 8.000,
     image: "1.jpeg",
   },
-  product2: {
+  "product2": {
     id: "404812c8-d734-4721-a14b-f9d63c767cec",
     name: "Fresas con crema 12oz",
     detail:"Toppings adicionales: Oreo, Cobertura de chocolate, Brownie, Arequipe.",
     price: 8.000,
     image: "2.jpeg",
   },
-  product3: {
+  "product3": {
     id: "404812c8-d734-4721-a14b-f9d63c767cec",
     name: "Fresas con crema 16oz",
     detail:"Toppings adicionales: Oreo, Cobertura de chocolate, Brownie, Arequipe.",
     price: 11.000,
     image: "2.jpeg",
   },
-  product4: {
+  "product4": {
     id: "981fc573-c999-4dbb-8653-0b991ec2d45a",
     name: "Cookies chips",
     detail:"Galletas con chips de chocolate.",
     price: 10.000,
     image: "4.jpeg",
   },
-  product5: {
+  "product5": {
     id: "6cf6fe50-d681-4523-9de9-82e6c03c37c0",
     name: "Brownies x4",
     detail:"Sabor: Oreo o Milo",
     price: 11.000,
     image: "5.jpeg",
   },
-  product6: {
+  "product6": {
     id: "a0038a80-7b5c-4971-977c-1ffe498cd7c9",
     name: "Cupcakes x6",
     detail:"Cupcakes con frosting de mantequilla: Vainilla, Naranja, Oreo, Chocolate",
