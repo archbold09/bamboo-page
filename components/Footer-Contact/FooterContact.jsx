b656fb1ab8d1f6bc3a0cfac990e10dc1798d63a7
angelArchbold <angelarchbold09@gmail.com>
2020-09-13 22:59:06 -0500

    config number footer contact

--- components/Footer-Contact/FooterContact.jsx
+++ components/Footer-Contact/FooterContact.jsx
@@ -9,7 +9,7 @@ const FooterContact = (props) => {
             </a>
           </li>
           <li className="mr-6 my-3">
-            <a className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1 px-3 rounded inline-flex items-center" target="blank" href="https://api.whatsapp.com/send?phone=3015320181">
+            <a className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1 px-3 rounded inline-flex items-center" target="blank" href="https://api.whatsapp.com/send?phone=573015320181&text=Hola!%20deseo%20m%C3%A1s%20informaci%C3%B3n!">
               <i className="mdi mdi-whatsapp wsp-icon"></i>
             </a>
           </li>
